99fc55072849c86e7bb232dd04876b60cf9f69a6
Sergey Karpov <karpov.srg@gmail.com>
2016-01-09 12:41:46 +0200

    Added: Spring configuration. Application config

--- src/main/java/org/hardcrystal/util/AppConfig.java
+++ src/main/java/org/hardcrystal/util/AppConfig.java
@@ -1,11 +1,22 @@
 package org.hardcrystal.util;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter{
+
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/");
+        internalResourceViewResolver.setSuffix(".jsp");
+
+        return internalResourceViewResolver;
+    }
 
 }
